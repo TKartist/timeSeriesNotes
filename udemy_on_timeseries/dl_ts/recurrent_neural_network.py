a302d94896219092b7867d875a958bf2ba1c95a6
TKartist <rimh@usi.ch>
2024-03-10 16:26:28 +0100

    built my first RNN model

--- udemy_on_timeseries/dl_ts/recurrent_neural_network.py
+++ udemy_on_timeseries/dl_ts/recurrent_neural_network.py
@@ -31,8 +31,8 @@ scaled_test = scaler.transform(test)
 # Neurons want certain format (batches) for time series data, so we convert it to the format with the following lib
 from keras.preprocessing.sequence import TimeseriesGenerator
 
-n_input = 2
-n_features = 1
+n_input = 12 # so we will use 12 months to predict the 13th month
+n_features = 1 # number of columns
 
 generator = TimeseriesGenerator(scaled_train, scaled_train, length=n_input, batch_size=1) # takes in a sequence of data-points gathered at equal
 # intervals, along with time series parameters such as stride, length of history, etc., to produce batches
@@ -48,9 +48,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 
-n_input = 12 # so we will use 12 months to predict the 13th month
-n_features = 1 # number of columns
-
 model = Sequential()
 model.add(LSTM(150, activation='relu', input_shape=(n_input, n_features))) # LSTM layer
 # add 150 neurons to LSTM (can be bigger or smaller but we have to find out manually by trying out different values)
@@ -58,7 +55,37 @@ model.add(Dense(1)) # output layer
 model.compile(optimizer='adam', loss='mse')
 
 model.fit_generator(generator, epochs=25)
-plt.plot(range(len(model.history.history['loss'])), model.history.history['loss'])
+# plt.plot(range(len(model.history.history['loss'])), model.history.history['loss'])
+# plt.show()
+
+# Forecasting with RNN model
+test_predictions = [] # holding predictions
+first_eval_batch = scaled_train[-n_input:]
+current_batch = first_eval_batch.reshape((1, n_input, n_features))
+
+for i in range(len(test)):
+    # predict one timestep ahead of historical 12 points
+    current_pred = model.predict(current_batch)[0] # result of predict is [[x]] so by calling [0], we get [x]
+
+    # store prediction
+    test_predictions.append(current_pred)
+
+    # update current batch to include the prediction
+    # prev shape of current batch (1, 12, 1)
+    # by calling current_batch[:,1:,:], we maintain all shape except the middle one
+    # second shape dimension will be from 1 to 12 instead of 0 to 12, ignoring first val of 2nd dimension
+    current_batch = np.append(current_batch[:,1:,:], [[current_pred]], axis=1)
+
+true_pred = scaler.inverse_transform(test_predictions)
+print(true_pred)
+print(n_input)
+test['Predictions'] = true_pred
+test.plot(figsize=(12, 8))
 plt.show()
 
+# saving trained model
+model.save('firstRNNModel.h5')
 
+# loading model
+from keras.models import load_model
+model1 = load_model('firstRNNModel.h5')
