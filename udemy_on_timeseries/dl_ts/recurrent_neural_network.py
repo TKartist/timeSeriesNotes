6a016bceca88cea8e3ec9ea308787ac5b72c2189
TKartist <rimh@usi.ch>
2024-03-10 16:02:58 +0100

    RNN with LSTM cell code

---
+++ udemy_on_timeseries/dl_ts/recurrent_neural_network.py
@@ -0,0 +1,64 @@
+# forecasting time series data with Keras and RNN
+import pandas as  pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+import warnings
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv("../../data/Alcohol_Sales.csv", index_col="DATE", parse_dates=True)
+df.index.freq = "MS"
+df.columns = ['Sales']
+
+# df.plot(figsize=(16, 10))
+# plt.show()
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+results = seasonal_decompose(df['Sales'])
+
+train = df.iloc[:313]
+test = df.iloc[313:]
+
+# we have to scale or normalize data for Neural Network because large difference cause confusion for neurons
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler.fit(train) # find the maximum, we find what to scale to basically
+scaled_train = scaler.transform(train)
+scaled_test = scaler.transform(test)
+
+# Neurons want certain format (batches) for time series data, so we convert it to the format with the following lib
+from keras.preprocessing.sequence import TimeseriesGenerator
+
+n_input = 2
+n_features = 1
+
+generator = TimeseriesGenerator(scaled_train, scaled_train, length=n_input, batch_size=1) # takes in a sequence of data-points gathered at equal
+# intervals, along with time series parameters such as stride, length of history, etc., to produce batches
+# for training/validation
+
+# essentially, len(scaled_train) - len(generator) = n_input
+# it is because "n_input" values are used to predict the next time-series value
+# hence if n_input = 2, and train data array is 313, we can try predicting 311 times
+
+# you can try manipulating the TimeseriesGenerator to see improvement of the performance
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+n_input = 12 # so we will use 12 months to predict the 13th month
+n_features = 1 # number of columns
+
+model = Sequential()
+model.add(LSTM(150, activation='relu', input_shape=(n_input, n_features))) # LSTM layer
+# add 150 neurons to LSTM (can be bigger or smaller but we have to find out manually by trying out different values)
+model.add(Dense(1)) # output layer
+model.compile(optimizer='adam', loss='mse')
+
+model.fit_generator(generator, epochs=25)
+plt.plot(range(len(model.history.history['loss'])), model.history.history['loss'])
+plt.show()
+
+
